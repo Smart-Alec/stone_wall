8948afed967caa86aacd3edcfcb23803e87e41ea
Smart_Alec <alecbartz02@outlook.com>
2022-08-02 18:09:15 -0500

    added logo

--- modInfo.lua
+++ modInfo.lua
@@ -1,7 +1,9 @@
 local modInfo = {
   name = "Stone Wall",
   description = "Adds a buildable stone wall.",
+  version = "b18v0.1",
   type = "world",
+  preview = "assets/preview.png",
   developer = "sennome, Jadi"
 }
 return modInfo
