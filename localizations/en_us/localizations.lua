80c6364b8483be0d0a3c25b0d101ed1af4317679
Smart_Alec <alecbartz02@outlook.com>
2022-08-02 12:36:25 -0500

    forgot to compile

---
+++ localizations/en_us/localizations.lua
@@ -0,0 +1,8 @@
+local localizations = { }
+localizations.values = {
+  object_stoneWall2x1 = "Small Stone Wall",
+  object_stoneWall2x1_plural = "Small Stone Walls",
+  object_build_stoneWall2x1 = "Small Stone Wall",
+  object_build_stoneWall2x1_plural = "Small Stone Walls"
+}
+return localizations
